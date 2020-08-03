89b40a1e1da38fb1afa908755e9a05cd0b599c4e
binds <binds@DESKTOP-0COA28K.hsd1.ca.comcast.net>
2020-08-02 22:27:02 -0700

    tester -Bindu Developed Login Page Class

---
+++ src/main/java/com/acti/pages/LoginPage.java
@@ -0,0 +1,47 @@
+package com.acti.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.acti.base.DriverScript;
+
+/*Tester Name:
+ * date:
+ * approved by:
+ * 
+ * 
+ * 
+ */
+
+public class LoginPage extends DriverScript
+{
+	@FindBy(id="username") WebElement usernameTbox;
+	@FindBy(name="pwd") WebElement passwordTbox;
+	@FindBy(id="loginButton") WebElement loginButton;
+	@FindBy(xpath="//div[@class='atLogoImg']") WebElement actiLogo;
+	
+//***********************************************************************************	
+	
+	public LoginPage() 
+	{
+		PageFactory.initElements(driver, this);
+	} 
+	
+	public void validateLoginFunction(String username, String password)
+	{
+		usernameTbox.sendKeys(username);
+		passwordTbox.sendKeys(password);
+		loginButton.click();
+	
+	}
+	
+	public boolean verifyActiLogo()
+	{
+		return actiLogo.isDisplayed();
+	}
+	
+	public String verifyLoginPageTitle(){
+		return driver.getTitle();
+	}
+}
